79f97b006f8dd0a65c3c372ad79244c03057c08b
Jago Van Dam <Jago.VanDam@sa.gov.au>
2022-09-07 13:24:05 +0930

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,59 @@
+function setIframe() {
+    var selection = document.getElementById("fig_select").value;
+    document.getElementById("figure-display").src = selection
+}
+
+function subButtonSetIframe(clickedButton) {
+  var subButtons = document.querySelectorAll("button.cod-sub-button");
+  subButtons.forEach(function(button) {
+    button.classList.remove('activeButton')
+  });
+  clickedButton.classList.add('activeButton');
+  document.getElementById("figure-display").src = clickedButton.value;
+}
+
+/*
+function openPanel() {
+    var panel = document.getElementById("panel");
+    var acc = document.getElementsByClassName("accordion")[0]
+    // Toggle between adding and removing the "active" class,
+    // to highlight the button that controls the panel 
+    acc.classList.toggle("active");
+    // Toggle between hiding and showing the active panel 
+    if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+}
+*/
+
+function openPanel(clicked) { 
+  var acc_id = clicked;
+  var acc = document.getElementById(acc_id);
+  var panel = acc.nextElementSibling;
+  
+  acc.classList.toggle("active");
+  //Toggle between hiding and showing the active panel
+  if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  
+}
+
+$(document).ready(function(){
+  $("#fig_select").on("change", function(){
+    switch($("#fig_select :selected").val()){
+      case "Figures/remote_number.html":
+        document.getElementById("buttonBlock").style.display = 'flex';
+        document.getElementById("b1").classList.add('activeButton');
+        document.getElementById("b2").classList.remove('activeButton');
+        break;
+      default:
+        document.getElementById("buttonBlock").style.display = 'none';
+    }
+    
+  })
+});
